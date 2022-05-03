9b226642bfbef0e56239898021ef1d1ce3ef568f
Denis Shalaev <pomarki@narod.ru>
2022-05-03 22:06:18 +0300

    22

--- components/ObjectCard/index.js
+++ components/ObjectCard/index.js
@@ -18,9 +18,45 @@ const ObjectCard = ({
 }) => {
   const [addressVisible, setAddressVisible] = useState(true);
   const [startDay, setStartDay] = useState(false);
+  const [expirationDay, setExpirationDay] = useState(false);
+  const [workDays, setWorkDays] = useState({ start: 0, finish: 0 });
 
   function chooseWorkingDate(choosenDay) {
-    setStartDay(choosenDay);
+    let durationWork = workDays.finish - workDays.start + 1;
+    let workObj = { start: 0, finish: 0 };
+
+    function workDayToggle() {
+      if (choosenDay < workDays.start) {
+        workObj = { start: choosenDay, finish: workDays.finish };
+
+        setWorkDays(workObj);
+      } else {
+        workObj = { start: workDays.start, finish: choosenDay };
+        setWorkDays(workObj);
+      }
+    }
+
+    if (workDays.start === 0) {
+      workObj = { start: choosenDay, finish: choosenDay };
+      setWorkDays(workObj);
+      return;
+    }
+
+    if (durationWork === 1) {
+      if (choosenDay === workDays.start) {
+        workObj = { start: 0, finish: 0 };
+        setWorkDays(workObj);
+        return;
+      } else {
+        workDayToggle();
+        return;
+      }
+    }
+
+    if (durationWork > 1) {
+      workDayToggle();
+      return;
+    }
   }
 
   let cardItemVisible;
@@ -102,10 +138,18 @@ const ObjectCard = ({
           {mode}
         </p>
         <div className={styles.cardWorkSchedule}>
-          <WorkSchedule month={"1"} fullMonth={fullMonth} onDayClick={chooseWorkingDate}/>
+          <WorkSchedule
+            month={"1"}
+            fullMonth={fullMonth}
+            onDayClick={chooseWorkingDate}
+          />
         </div>
         <div className={styles.cardTotal}>
-          <ObjectCardTotal start={startDay} end={"99"} team={"v"} />
+          <ObjectCardTotal
+            start={workDays.start}
+            end={workDays.finish}
+            team={"v"}
+          />
         </div>
       </div>
     </>
